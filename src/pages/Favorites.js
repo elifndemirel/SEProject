a257bb571a863fa773d9eca0ba46ee918e7dd8b8
Cahid Alır <alircahid@gmail.com>
2021-12-25 20:29:31 +0300

    Search bar added to fav page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 import NavBar from "../components/NavBar";
 
 export default class Favorites extends Component {
-    render() {
+            render() {
         return (
             <div>
                 <NavBar/>
-                    <div>
-
-                    </div>
+      <div
+        className="pt-2"
+        style={{ display: "table", marginLeft: "auto", marginRight: "auto" }}
+      >
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          //onChange={(event) => setFilteredData(event.target.value)}
+        />
+      </div>
             </div>
         )
-    }
+       }
 }
+
