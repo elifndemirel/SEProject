6626b8dd01948587758be3eb535d161f784d4c9c
Elif Nur Demirel <eliffnurdemirell35@gmail.com>
2021-12-27 20:27:17 +0300

    profile page added

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,22 +1,109 @@
 import NavBar from "../components/NavBar";
 import { Navigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "alertifyjs/build/css/alertify.css";
+import { useEffect } from "react";
 
 const Profile = () => {
-  const onSubmit = (e) => {
-    e.preventDefault();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [oldPassword, setOldPassword] = useState("");
+  const [city, setCity] = useState("");
 
-    console.log(e.target.firstname.value);
-    console.log(e.target.lastname.value);
-    console.log(e.target.email.value);
-    console.log(e.target.oldPassword.value);
-    console.log(e.target.newPassword.value);
-    console.log(e.target.confirmPassword.value);
+  const [formErrors, setformErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setformErrors(validate(firstName));
+    setformErrors(validate(lastName));
+    setformErrors(validate(username));
+    setformErrors(validate(password));
+    setformErrors(validate(newPassword));
+    setformErrors(validate(oldPassword));
+    setformErrors(validate(city));
+
+    setIsSubmit(true);
   };
 
-  if(sessionStorage.getItem("response")!=200){
-    return <Navigate to="/"/>
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(username);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+
+    if (!firstName) {
+      errors.firstName = "First name is required!";
+    } else if (firstName.length < 3) {
+      errors.firstName = "First name must be more than 2 characters!";
+    } else if (firstName.length > 25) {
+      errors.firstName = "First name cannot exceed more than 25 characters!";
+    }
+
+    if (!lastName) {
+      errors.lastName = "Last name is required!";
+    } else if (lastName.length < 2) {
+      errors.lastName = "Last name must be more than 1 character!";
+    } else if (lastName.length > 25) {
+      errors.lastName = "Last name cannot exceed more than 25 characters!";
+    }
+    if (!username) {
+      errors.username = "Username is required!";
+    } else if (username.length < 3) {
+      errors.username = "Username must be more than 2 characters!";
+    } else if (username.length > 20) {
+      errors.username = "Username cannot exceed more than 20 characters!";
+    }
+    if (!oldPassword) {
+      errors.oldPassword = "Old password is required!";
+    } else if (oldPassword.length < 6) {
+      errors.oldPassword = "Old password must be more than 5 characters!";
+    } else if (oldPassword.length > 30) {
+      errors.oldPassword =
+        "Old password cannot exceed more than 30 characters!";
+    }
+    if (!newPassword) {
+      errors.newPassword = "New password is required!";
+    } else if (newPassword.length < 6) {
+      errors.newPassword = "New password must be more than 5 characters!";
+    } else if (newPassword.length > 30) {
+      errors.newPassword =
+        "New password cannot exceed more than 30 characters!";
+    }
+    if (!password) {
+      errors.password = "Confirmed password is required!";
+    } else if (password.length < 6) {
+      errors.password = "Confirmed password must be more than 5 characters!";
+    } else if (password.length > 30) {
+      errors.password =
+        "Confirmed password cannot exceed more than 30 characters!";
+    } else if (newPassword != password) {
+      errors.password = "This does not match with your new password!";
+    }
+
+    if (!city) {
+      errors.city = "City is required!";
+    } else if (city.length < 2) {
+      errors.city = "City must be more than 1 character!";
+    } else if (city.length > 40) {
+      errors.city = "City cannot exceed more than 40 characters!";
+    }
+    return errors;
+  };
+
+  if (sessionStorage.getItem("response") != 200) {
+    return <Navigate to="/" />;
   }
-  
+
   return (
     <div>
       <NavBar />
@@ -24,7 +111,7 @@ const Profile = () => {
         <div className="row justify-content-center">
           <div className="col-12 col-lg-10 col-xl-8 mx-auto">
             <div className="my-4">
-              <form onSubmit={onSubmit}>
+              <form>
                 <hr className="my-4" />
                 <div className="form-row">
                   <div className="form-group col-md-6">
@@ -36,8 +123,13 @@ const Profile = () => {
                       id="firstname"
                       name="firstname"
                       className="form-control mb-2"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
                     />
                   </div>
+                  <p style={{ color: "#B63B20" }}>
+                    <b>{formErrors.firstName}</b>
+                  </p>
                   <div className="form-group col-md-6">
                     <label for="lastname" className="mb-2">
                       Last Name
@@ -47,8 +139,13 @@ const Profile = () => {
                       id="lastname"
                       name="lastname"
                       className="form-control mb-2"
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
                     />
                   </div>
+                  <p style={{ color: "#B63B20" }}>
+                    <b>{formErrors.lastName}</b>
+                  </p>
                   <div className="form-group col-md-6">
                     <label for="userName" className="mb-2">
                       Username
@@ -58,8 +155,13 @@ const Profile = () => {
                       className="form-control mb-2"
                       id="userName"
                       name="username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
                     />
                   </div>
+                  <p style={{ color: "#B63B20" }}>
+                    <b>{formErrors.username}</b>
+                  </p>
                   <div className="form-group col-md-6">
                     <label for="city" className="mb-2">
                       City
@@ -69,9 +171,15 @@ const Profile = () => {
                       id="city"
                       name="city"
                       className="form-control mb-2"
+                      value={city}
+                      onChange={(e) => setCity(e.target.value)}
                     />
                   </div>
+                  <p style={{ color: "#B63B20" }}>
+                    <b>{formErrors.city}</b>
+                  </p>
                 </div>
+
                 <hr className="my-5" />
                 <div className="row mb-4">
                   <div className="col-md-6">
@@ -84,8 +192,13 @@ const Profile = () => {
                         className="form-control mb-2"
                         id="oldPassword"
                         name="oldPassword"
+                        value={oldPassword}
+                        onChange={(e) => setOldPassword(e.target.value)}
                       />
                     </div>
+                    <p style={{ color: "#B63B20" }}>
+                      <b>{formErrors.oldPassword}</b>
+                    </p>
                     <div className="form-group">
                       <label for="newPassword" className="mb-2">
                         New Password
@@ -95,8 +208,13 @@ const Profile = () => {
                         className="form-control mb-2"
                         id="newPassword"
                         name="newPassword"
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}
                       />
                     </div>
+                    <p style={{ color: "#B63B20" }}>
+                      <b>{formErrors.newPassword}</b>
+                    </p>
                     <div className="form-group">
                       <label for="confirmPassword" className="mb-2">
                         Confirm Password
@@ -106,12 +224,20 @@ const Profile = () => {
                         className="form-control mb-2"
                         id="confirmPassword"
                         name="confirmPassword"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                       />
                     </div>
+                    <p style={{ color: "#B63B20" }}>
+                      <b>{formErrors.password}</b>
+                    </p>
                   </div>
                 </div>
                 <div className="text-center">
-                  <button type="submit" className="btn btn-primary mt-2 mb-5">
+                  <button
+                    className="btn btn-primary mt-2 mb-5"
+                    onClick={handleSubmit}
+                  >
                     Save Change
                   </button>
                 </div>
